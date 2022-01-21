acabce26a726dad3e8500f20143917b13e4e8710
Monalisa Nayak <monalisa.nayak@nexval.com>
2022-01-21 14:16:09 +0530

    Network bandwidth

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Network_bandwidth
+# Network Bandwidth Detection
+
---
+++ myNetworkLibrary/src/main/java/com/internetcheck/mynetworklibrary/ITrafficSpeedListener.java
@@ -0,0 +1,6 @@
+package com.internetcheck.mynetworklibrary;
+
+public interface ITrafficSpeedListener {
+
+    void onTrafficSpeedMeasured(double upStream, double downStream);
+}
